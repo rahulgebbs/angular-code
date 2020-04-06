ade7323dca0d2a237f5825211f3dd462a89f1cc1
Rahul M <rahul.mutyalkar@gebbs.com>
2020-04-06 12:55:21 +0530

    add de-allocate page with static ui

--- src/app/concluder-report/concluder-report.component.ts
+++ src/app/concluder-report/concluder-report.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./concluder-report.component.scss']
 })
 export class ConcluderReportComponent implements OnInit {
+  title="Concluder Report";
   fromDate = new Date();
   maxDate = new Date();
   gridApi;
@@ -18,17 +19,7 @@ export class ConcluderReportComponent implements OnInit {
     { headerName: 'User Name', field: 'Username' },
     { headerName: 'Full Name', field: 'Full_Name' },
     { headerName: 'Email Id', field: 'Email_Id' },
-    { headerName: 'Process', field: 'CommaSeparatedClients' },
-    // { headerName: 'Role', field: 'Role' },
-    // { headerName: 'Activate', field: 'Is_Deactivated', cellRenderer: this.isAcitvate },
-    // { headerName: 'Locked', field: 'Is_Locked', cellRenderer: this.ValCheck },
-    // { headerName: 'Terminated', field: 'Is_Terminated', cellRenderer: this.ValCheck },
-    // { headerName: 'User Type', field: 'User_Type' },
-    // { headerName: 'Expertise_In_Call', field: 'Expertise_In_Call', cellRenderer: this.ValCheck },
-    // { headerName: 'Expertise_In_Website', field: 'Expertise_In_Website', cellRenderer: this.ValCheck },
-    // { headerName: 'Expertise_In_Email', field: 'Expertise_In_Email', cellRenderer: this.ValCheck },
-    // { headerName: 'Expertise_In_Fax', field: 'Expertise_In_Fax', cellRenderer: this.ValCheck },
-    // { headerName: 'URL', field: 'URL', cellRenderer: this.ValCheck },
+    { headerName: 'Process', field: 'CommaSeparatedClients' }
   ];
   rowSelection = "single";
   constructor(
@@ -39,16 +30,16 @@ export class ConcluderReportComponent implements OnInit {
     this.rowData = [];
   }
   onGridReady(params) {
-    // this.gridApi = params.api;
-    // this.gridColumnApi = params.columnApi;
-    // params.columnApi.autoSizeColumns();
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    params.columnApi.autoSizeColumns();
   }
   autoSizeAll() {
     var allColumnIds = [];
-    // this.gridColumnApi.getAllColumns().forEach(function (column) {
-    //   allColumnIds.push(column.colId);
-    // });
-    // this.gridColumnApi.autoSizeColumns(allColumnIds);
+    this.gridColumnApi.getAllColumns().forEach(function (column) {
+      allColumnIds.push(column.colId);
+    });
+    this.gridColumnApi.autoSizeColumns(allColumnIds);
   }
   onCellClicked(e) {}
 }
--- src/app/deallocate-concluder/deallocate-concluder.component.ts
+++ src/app/deallocate-concluder/deallocate-concluder.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-deallocate-concluder',
   templateUrl: './deallocate-concluder.component.html',
   styleUrls: ['./deallocate-concluder.component.css']
 })
 export class DeallocateConcluderComponent implements OnInit {
-
-  constructor() { }
+  title = "De-Allocate Concluder";
+  fromDate = new Date();
+  maxDate = new Date();
+  gridApi;
+  gridColumnApi;
+  rowData;
+  columnDefs = [
+    {
+      headerName: '#', width: 130, checkboxSelection: true, suppressSorting: true,
+      suppressMenu: true, pinned: true
+    },
+    { headerName: 'ID', field: 'Id', hide: "false" },
+    { headerName: 'Employee', field: 'Employee' },
+    { headerName: 'Non Concluder Accounts', field: 'Count' }
+  ];
+  rowSelection = "single";
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.rowData = [{
+      Employee: 121,
+      Count: 6
+    }, {
+      Employee: 122,
+      Count: 5
+    }, {
+      Employee: 123,
+      Count: 1
+    }];
   }
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    params.columnApi.autoSizeColumns();
+  }
+  autoSizeAll() {
+    var allColumnIds = [];
+    this.gridColumnApi.getAllColumns().forEach(function (column) {
+      allColumnIds.push(column.colId);
+    });
+    this.gridColumnApi.autoSizeColumns(allColumnIds);
+  }
+  onCellClicked(e) { }
 
 }
