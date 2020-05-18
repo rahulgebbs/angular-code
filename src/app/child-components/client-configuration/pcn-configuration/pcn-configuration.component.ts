af76a2edef049dda71f40467f935fe4a1f6a76ac
Rahul M <rahul.mutyalkar@gebbs.com>
2020-05-19 04:48:50 +0530

    add reactive form in pcn configuration page in client configuration

--- src/app/child-components/client-configuration/pcn-configuration/pcn-configuration.component.ts
+++ src/app/child-components/client-configuration/pcn-configuration/pcn-configuration.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { SaagService } from '../../../service/client-configuration/saag.service';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+
+
 import { Token } from '../../../manager/token';
 import { NotificationService } from 'src/app/service/notification.service';
 import * as FileSaver from 'file-saver';
 import { Router } from '@angular/router';
 import { ResponseHelper } from 'src/app/manager/response.helper';
-import { ExcelService } from 'src/app/service/client-configuration/excel.service';
-import { customValidation } from 'src/app/manager/customValidators';
-// import { dropDownFields } from 'src/app/manager/dropdown-feilds';
 import { PcnService } from 'src/app/service/pcn.service';
 @Component({
   selector: 'app-pcn-configuration',
@@ -27,11 +25,14 @@ export class PcnConfigurationComponent implements OnInit {
   showPopup;
   @Input() ClientData;
   @Output() next_page = new EventEmitter<any>();
-  columnDefs = [];
-  gridApi;
-  gridColumnApi;
-  rowSelection = "single";
-  constructor(private router: Router, private notificationservice: NotificationService, private pcnService: PcnService) {
+  // columnDefs = [];
+  // gridApi;
+  // gridColumnApi;
+  // rowSelection = "single";
+  addPCNForm;
+  pcnList: any;
+  // constructor(private fb: FormBuilder) { }
+  constructor(private router: Router, private notificationservice: NotificationService, private pcnService: PcnService, private fb: FormBuilder) {
     this.token = new Token(this.router);
     this.userData = this.token.GetUserData();
   }
@@ -41,29 +42,46 @@ export class PcnConfigurationComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initForm();
     this.getPCNList();
     this.ResponseHelper = new ResponseHelper(this.notificationservice);
-
-    this.columnDefs = [
-      { headerName: 'Display Header', field: 'Display_Header', editable: true },
-      { headerName: 'Field Limit', field: 'Field_Limit', editable: true },
-      {
-        field: "Column_Data_Type",
-        headerName: "Column Data Type",
-        editable: true
-      }];
   }
   nextPage() {
     this.next_page.emit('pcn');
   }
   onGridReady(params) {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-    params.columnApi.autoSizeColumns();
+    // this.gridApi = params.api;
+    // this.gridColumnApi = params.columnApi;
+    // params.columnApi.autoSizeColumns();
   }
 
   navigate() {
     this.next_page.emit('pcn');
+  }
+
+  initForm() {
+    this.addPCNForm = this.fb.group({
+      pcnList: this.fb.array([])
+    });
+  }
+  createItem(pcn) {
+    // console.log('addPCNForm : ', this.addPCNForm);
+    return this.fb.group({
+      "Display_Header": [pcn.Display_Header, Validators.required],
+      "Column_Data_Type": [pcn.Column_Data_Type, Validators.required],
+      "Field_Limit": [pcn.Field_Limit, [Validators.required, Validators.pattern(/^(\d|,)*\d*$/)]]
+    });
+  }
+
+  addItem(pcn): void {
+    this.pcnList = this.addPCNForm.get('pcnList') as FormArray;
+    this.pcnList.push(this.createItem(pcn));
+    console.log('this.pcnList : ', this.pcnList);
+    const lastIndex = this.pcnList.length - 1;
+    setTimeout(() => {
+      this.checkIfAlreadyExist(this.pcnList.controls[lastIndex].controls.Display_Header, lastIndex);
+    }, 100);
+
   }
 
   getPCNList() {
@@ -71,41 +89,77 @@ export class PcnConfigurationComponent implements OnInit {
     this.rowData = null;
     this.pcnService.getPCNList(this.ClientData.Id).subscribe((response: any) => {
       console.log('response : ', response.Data);
-      this.rowData = response.Data
+      this.rowData = response.Data;
+      this.setFormFields();
     }, (error) => {
       this.rowData = [];
       console.log('error : ', error)
     });
   }
 
+  setFormFields() {
+    this.rowData.forEach((pcn) => {
+      this.addItem(pcn)
+    });
+  }
+
   openPopup() {
     this.showPopup = true;
   }
+
   closePopup(event) {
     console.log('closePopup(event) : ', event)
     this.showPopup = false;
   }
+
   saveFields(value) {
     console.log('pcn value : ', value);
-    this.rowData = this.rowData ? this.rowData : []
-    this.rowData.push(value);
-    this.rowData = JSON.parse(JSON.stringify(this.rowData));
-    console.log('this.rowData : ', this.rowData);
+    if (value) {
+      this.addItem(value);
+    }
     this.showPopup = false;
+    // this.rowData = this.rowData ? this.rowData : []
+    // this.rowData.push(value);
+    // this.rowData = JSON.parse(JSON.stringify(this.rowData));
+    // console.log('this.rowData : ', this.rowData);
+    // this.showPopup = false;
   }
 
   submit() {
     console.log('submit() : ', this.rowData);
     // this.next_page.emit('pcn');
+    if (this.addPCNForm && !this.addPCNForm.valid) {
+      return false;
+    }
     const obj = {
       "Client_Id": this.ClientData.Id,
-      "Client_PCN_Configuration_List": this.rowData
+      "Client_PCN_Configuration_List": this.addPCNForm.value.pcnList
     }
     this.pcnService.updatePCNList(obj).subscribe((response) => {
       console.log('response :', response)
     }, (error) => {
       console.log('error :', error)
     })
+  }
+
+  checkIfAlreadyExist(field, fieldIndex) {
+    console.log('checkIfAlreadyExist : ', field, fieldIndex)
+    const pcnList = this.addPCNForm.value.pcnList;
+    var matchedElement;
+    matchedElement = pcnList.find((element, index) => {
+      if (field.value.length > 0 && fieldIndex != index) {
+        return field.value == element.Display_Header;
+      }
+    });
+    if (matchedElement != null) {
+      console.log('matchedElement : ', matchedElement)
+      field.setErrors({ 'incorrect': true });
+      field.markAsDirty();
+    }
+    else {
+      if (field.value.length > 0)
+        field.setErrors(null);
+    }
   }
   removeItem(i) {
     this.rowData.splice(i, 1)
