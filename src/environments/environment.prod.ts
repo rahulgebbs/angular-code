e19761f724a9854d0c536078444c07f2e72edab6
Pushkar Kolte <pkolte@GHS.LOCAL>
2021-04-21 15:46:49 +0530

    chnage version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,9 +10,9 @@ export const environment = {
   // linkUsed: '172.19.9.101:1001'
 
   //External Live
-  // ApiUrl: "https://www.gebbsrcm.com:1008/", //For CLIENT EXTERNAL LINK
-  // FileApiUrl: "https://www.gebbsrcm.com:1009/",// LIVE Server,
-  // linkUsed: 'www.gebbsrcm.com'
+  ApiUrl: "https://www.gebbsrcm.com:1008/", //For CLIENT EXTERNAL LINK
+  FileApiUrl: "https://www.gebbsrcm.com:1009/",// LIVE Server,
+  linkUsed: 'www.gebbsrcm.com'
 
   // LIVE Server
   // ApiUrl: "http://172.18.7.2:1001/", // LIVE Server,
@@ -20,9 +20,9 @@ export const environment = {
   // linkUsed: '172.18.7.2:1002' 
 
   // NEW UAT
-  FileApiUrl: "http://172.30.52.25:1007",
-  ApiUrl: "http://172.30.52.25:1001/",
-  linkUsed: '172.30.52.25:1001'
+  // FileApiUrl: "http://172.30.52.25:1007",
+  // ApiUrl: "http://172.30.52.25:1001/",
+  // linkUsed: '172.30.52.25:1001'
 
   // NEW Bootstrap UAT Link
   // FileApiUrl: "http://172.30.52.25:1114",
